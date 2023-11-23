21e11cecd3eb6e461f5892cab2e9441e77c3ebfa
Albatrous <yasridho@gmail.com>
2023-11-23 19:02:32 +0700

    Resolve #4 to redirect to the appropriate page (login or home page)

--- lib/app/modules/splash_screen/controllers/splash_screen_controller.dart
+++ lib/app/modules/splash_screen/controllers/splash_screen_controller.dart
@@ -1,5 +1,16 @@
+import 'dart:async';
+
 import 'package:get/get.dart';
+import 'package:pasar_petani/app/routes/app_pages.dart';
+import 'package:pasar_petani/config/constants.dart';
 
 class SplashScreenController extends GetxController {
-  // TODO implement SplashScreenController
+  redirect() => Timer.periodic(const Duration(seconds: 3), (timer) {
+        if (storage.read('access_token') != null) {
+          Get.offNamed(Routes.HOME);
+        } else {
+          Get.offNamed(Routes.LOGIN);
+        }
+        timer.cancel();
+      });
 }
